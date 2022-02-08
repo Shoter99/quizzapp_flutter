7a5b178fb4efc3e6a87060fe44a74a2c57ece336
Shoter99 <shoter998@gmail.com>
2022-02-08 07:45:58 +0100

    Stream

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quizapp/shared/bottom_nav.dart';
 import 'package:quizapp/services/firestore.dart';
 import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/loading.dart';
+import 'package:quizapp/topics/topic_item.dart';
 
 class TopicsPage extends StatelessWidget {
   const TopicsPage({Key? key}) : super(key: key);
@@ -12,7 +14,7 @@ class TopicsPage extends StatelessWidget {
         future: FirestoreService().getTopics(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Loading...'));
+            return const Loading();
           } else if (snapshot.hasError) {
             return const Center(child: Text("error"));
           } else if (snapshot.hasData) {
@@ -27,7 +29,8 @@ class TopicsPage extends StatelessWidget {
                 padding: const EdgeInsets.all(20),
                 crossAxisSpacing: 10,
                 crossAxisCount: 2,
-                children: topics.map((topic) => Text(topic.title)).toList(),
+                children:
+                    topics.map((topic) => TopicItem(topic: topic)).toList(),
               ),
               bottomNavigationBar: const BottomNavBar(),
             );
